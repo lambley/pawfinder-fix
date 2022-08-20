78d5031b5f3e24028709619b7b0316aa059c5e89
Aaron Lambley <aaron.lambley@gmail.com>
2022-08-20 15:11:54 +0100

    add missing colon in route file

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   end
   resources :dogs, only: %i[index show new create]
   resources :activities, only: %i[index new create]
-  resources :locations, only %i[index new create]
+  resources :locations, only: %i[index new create]
 end
