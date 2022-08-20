349002079a1583f1fb37f16fee3dfdfe361f061c
Aaron Lambley <aaron.lambley@gmail.com>
2022-08-20 11:46:16 +0100

    create dog controller and update routes

---
+++ app/controllers/dog_controller.rb
@@ -0,0 +1,31 @@
+class DogController < ApplicationController
+  def index
+    # change this later to index only for current_user
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new()
+    @user = current_user
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user = current_user
+    if @dog.save
+      redirect_to user_path(current_user), status: :see_other
+    else
+      redirect_to dog_new_path, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name,:breed, :colour, :age, :biography)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'dog/index'
+  get 'dog/show'
+  get 'dog/new'
+  get 'dog/create'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :dogs, only: %i[index show new create]
 end
