bb883e40b9289f60929a6928fe6d3fab5f1308d4
Aaron Lambley <aaron.lambley@gmail.com>
2022-08-20 11:30:09 +0100

    made rating optional

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,5 @@ Rails.application.routes.draw do
   get 'reviews/update'
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :activities only: %i[]
 end
