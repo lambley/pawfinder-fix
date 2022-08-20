fbd0ae70d241aa9eb29777ebeb98dfd624a237dc
Aaron Lambley <aaron.lambley@gmail.com>
2022-08-20 11:39:57 +0100

    add review controller methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,33 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new()
+    @user = current_user
+    @activity = Activity.find(params[:activity_id])
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.activity = Activity.find(params[:activity_id])
+    if @review.save
+      redirect_to activities_path, status: :see_other
+    else
+      redirect_to new_activity_review_path, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
 
   def update
+    @review.update(review_params)
+    redirect_to activities_path, status: :ok
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'reviews/update'
   devise_for :users
   root to: "pages#home"
-  resources :activities only: %i[]
+  resources :activities, only: %i[index new create] do
+    resources :reviews, only: %i[new create edit update]
+  end
 end
