c3a78855c8bf424d16b6f5ddf17e0ba0be3edcc0
Norbert Csipak <norbertcipak@me.com>
2022-08-20 12:08:42 +0100

    locations controller with create new and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "locations#index", as: :locations
+  get "/locations", to: "locations#index"
+  get "locations/new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
