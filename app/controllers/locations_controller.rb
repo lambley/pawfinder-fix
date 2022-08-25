1fa7177166a5e2a536f6282d19891cbf753849b8
Kyle Mobley <skyruel@gmail.com>
2022-08-25 20:29:02 +0100

    Updated locations controller with mapbox

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,13 @@
 class LocationsController < ApplicationController
   def index
     @locations = Location.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @locations.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   def new
