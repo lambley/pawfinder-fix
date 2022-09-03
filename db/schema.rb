b9d2ab27d37b25cfd792176db3d0d4ac6b89564f
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-03 15:16:56 +0100

    add cl_tag to dogs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_03_140229) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_03_141254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_03_140229) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cl_tag"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
