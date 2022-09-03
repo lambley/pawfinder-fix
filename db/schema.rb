ca0bce1c9aa3ace6cd7c457c60df8b0d25053c5d
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-03 15:23:24 +0100

    add cl_tag to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_03_141254) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_03_141828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -103,6 +103,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_03_141254) do
     t.string "first_name"
     t.string "last_name"
     t.text "biography"
+    t.string "cl_tag"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
