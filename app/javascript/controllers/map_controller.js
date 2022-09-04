f3640dda6522bde783a2c6564fbe74d5fe6ec624
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-04 14:22:35 +0100

    updated style

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -11,10 +11,11 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v11"
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+    this.map.addControl(new mapboxgl.NavigationControl());
   }
 
   #addMarkersToMap() {
@@ -26,6 +27,7 @@ export default class extends Controller {
   }
 
   #fitMapToMarkers() {
+    console.log("fit to map");
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
