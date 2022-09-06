f49ff094549152af1dd17ebc250dc33da6fea94f
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-06 21:26:24 +0100

    update favouriting routes and controller actions

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,7 +1,12 @@
 class FavouritesController < ApplicationController
   def create
+    activity = Activity.find(params[:activity_id])
+    Favourite.create!(favouritable: activity, user_id: current_user.id)
   end
 
   def destroy
+    favourite = Favourite.find(params[:id])
+    favourite.destroy
+
   end
 end
