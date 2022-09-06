3ab82b6c28c2c3102329efd5b3bcc0f54888fc84
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-06 21:25:55 +0100

    generate stimulus controller for favouriting

---
+++ app/javascript/controllers/favourite_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="favourite"
+export default class extends Controller {
+  static targets = [ "heart" ]
+
+  connect() {
+    console.log("fav connected");
+  }
+
+  addToFavourites() {
+    // check if empty heart (not current favourite)
+    if (this.heartTarget.classList.contains("fa-regular")) {
+      this.heartTarget.classList.toggle("fa-regular")
+      this.heartTarget.classList.toggle("fa-solid")
+      alert(`Added to Favourites`)
+    }
+    // check if full heart (favourited)
+    else if (this.heartTarget.classList.contains("fa-solid")) {
+      this.heartTarget.classList.toggle("fa-solid")
+      this.heartTarget.classList.toggle("fa-regular")
+      alert(`Removed from Favourites`)
+    }
+  }
+}
