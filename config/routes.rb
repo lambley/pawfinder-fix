bc9c9d38385919806cbcdbf86186433efea27247
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-07 13:00:00 +0100

    add route for user to see their reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  # custom routes for home page search form AJAX
   get "/restaurant-search", to: "pages#restaurant"
   get "/park-search", to: "pages#park"
   get "/bin-search", to: "pages#bin"
   get "/dog-search", to: "pages#dog"
+  # route for current user profile page
   get "/profile", to: "pages#profile"
+  # route for current user review index
+  get "/profile/reviews", to: "pages#profile_reviews"
   devise_for :users
   resources :activities, only: %i[index new create] do
     resources :reviews, only: %i[index new create edit update]
