86fbe47558571034ea95e465473a7459c7caffd9
laura reimarte <reimarte.laura@gmail.com>
2022-09-10 13:43:50 +0100

    styled the review page

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :activity
   # validations
-  validates :content, length: { minimum: 6 }
-  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
+  validates :content, length: { minimum: 6 }, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }, presence: true
 end
