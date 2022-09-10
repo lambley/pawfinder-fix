de29fecded588f2e4cc1cb0952e21c74473407ab
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-10 09:37:35 +0100

    change all review ratings to X out of 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ApplicationRecord
   belongs_to :activity
   # validations
   validates :content, length: { minimum: 6 }
-  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 10 }
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
 end
