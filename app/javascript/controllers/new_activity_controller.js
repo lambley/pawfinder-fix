43b3d6aee1e885af1eb0e80f476182df44075213
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-17 10:43:52 +0100

    js to change image on form load. change to cl image tag

--- app/javascript/controllers/new_activity_controller.js
+++ app/javascript/controllers/new_activity_controller.js
@@ -9,19 +9,49 @@ export default class extends Controller {
   }
 
   showForm() {
+    const newFormImageElement = document.getElementById("new-form-img")
+    const parkImage = "new-park.jpg"
+    const restaurantImage = "new-restaurant.jpg"
+    const binImage = "new-bin.jpg"
     switch(this.selectTarget.value.toLowerCase()) {
       case "park":
+        newFormImageElement.setAttribute(
+          'src',
+          this.#amendCloudinaryUrl(
+            newFormImageElement.getAttribute('src'),
+            parkImage
+          )
+        )
         this.parkFormTarget.classList.remove("display-none")
         break
       case "restaurant":
+        newFormImageElement.setAttribute(
+          'src',
+          this.#amendCloudinaryUrl(
+            newFormImageElement.getAttribute('src'),
+            restaurantImage
+          )
+        )
         this.restaurantFormTarget.classList.remove("display-none")
         break
       case "dog bin":
+        newFormImageElement.setAttribute(
+          'src',
+          this.#amendCloudinaryUrl(
+            newFormImageElement.getAttribute('src'),
+            binImage
+          )
+        )
         this.binFormTarget.classList.remove("display-none")
         break
       default:
         console.log("error in drop down selection");
     }
     document.getElementById("new-activity-selector").classList.add("display-none")
+  }
+
+  #amendCloudinaryUrl(url, newFileName) {
+    const substring = url.substring(0, url.lastIndexOf("/") + 1)
+    return `${substring}/${newFileName}`
   }
 }
