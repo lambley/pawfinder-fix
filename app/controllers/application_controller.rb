96528e48bed5033a8f743f3ded5e0e2c51bf5219
Norbert Csipak <norbertcipak@me.com>
2022-09-17 10:54:24 +0100

    new user can sign up with real informations, user can edit their info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :biography])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :biography])
   end
 end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,7 @@
+# class Users::RegistrationsController < Devise::RegistrationsController
+#   private
+
+#   def sign_up_params
+#     params.require(:user).permit(:first_name, :last_name, :biography)
+#   end
+# end
