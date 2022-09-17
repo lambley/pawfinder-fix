52330cfe58021913d41fa6cf9e623ad6ca30544d
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-17 15:54:22 +0100

    scroll to card from mapbox pin

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="scroll"
+export default class extends Controller {
+  static targets = [ "activity" ]
+  connect() {
+    console.log("scroll controller connected");
+  }
+
+  scrollIntoView(event) {
+    event.preventDefault()
+    const scrollElement = document.getElementById(this.activityTarget.id.replace(/popup-/g,''))
+    scrollElement.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+  }
+}
